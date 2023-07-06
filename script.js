907d5578014bedf6db1ede778dd35b843018c73b
BobiTrpevski <bobitrpevski94@gmail.com>
2023-07-06 23:29:40 +0200

    Changed ajax request properties to match github pages

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 $("#createLeadBtn").click(function() {
+  let apikey = "MTIzMDka02efd5b36cb3a55fccc71d09e036a31";
+  let redirectLink = "https://bobitrpevski.github.io/PabauExercise";
+  let firstName = $("#leadFirstName").val();
+  let lastName = $("#leadLastName").val();
+  let email = $("#leadEmail").val();
+
   $.ajax({
-    url: `https://migration.pabau.me/OAuth2/leads/lead-curl.php?api_key=MTIzMDka02efd5b36cb3a55fccc71d09e036a31&redirect_link=https://amazon.com/lead&Fname=${$("#leadFirstName").val()}&Lname=${$("#leadLastName").val()}&email=${$("#leadEmail").val()}`,
+    url: `https://migration.pabau.me/OAuth2/leads/lead-curl.php?api_key=${apikey}&redirect_link=${redirectLink}&Fname=${firstName}firstName&Lname=${lastName}&email=${email}`,
     type: "POST",
     dataType: 'jsonp'
-  })
+  });
+  
   return false;
 });
