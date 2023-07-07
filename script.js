b8925913e01b5effc187ecaaba15cd9a2b1cafcf
BobiTrpevski <bobitrpevski94@gmail.com>
2023-07-07 02:03:03 +0200

    Changed ajax request to form submition for lead creation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $("#createLeadBtn").click(function() {
   let apikey = "MTIzMDka02efd5b36cb3a55fccc71d09e036a31";
-  let redirectLink = "https://bobitrpevski.github.io/PabauExercise";
+  let redirectLink = "https://bobitrpevski.github.io/PabauExercise/";
   let firstName = $("#leadFirstName").val();
   let lastName = $("#leadLastName").val();
   let email = $("#leadEmail").val();
 
   $.ajax({
-    url: `https://migration.pabau.me/OAuth2/leads/lead-curl.php?api_key=${apikey}&redirect_link=${redirectLink}&Fname=${firstName}firstName&Lname=${lastName}&email=${email}`,
+    url: `https://migration.pabau.me/OAuth2/leads/lead-curl.php?api_key=${apikey}&redirect_link=${redirectLink}&Fname=${firstName}&Lname=${lastName}&email=${email}`,
     type: "POST",
     dataType: 'jsonp',
     success: function (response) {
